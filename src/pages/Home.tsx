0b5841a6a30801fd124e1d6822b822fbba77a116
Uttam Sutariya <pateluttam171210@gmail.com>
2023-05-06 13:21:22 +0530

    docker file added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 const Home = () => {
 	return (
 		<div className="mt-5 text-white">
-			<h1>React + Typescript Shopping Cart</h1>
+			<h1>React with Typescript Shopping Cart</h1>
 			<h3>
 				<Link to="/store">Go to store</Link>
 			</h3>
